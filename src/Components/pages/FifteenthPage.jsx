5bd7da7af954d0c3536aa3d07765d12a4ce9001b
chrisloui <cacanete@up.edu.ph>
2024-10-16 19:23:33 +0800

    last

--- src/Components/pages/FifteenthPage.jsx
+++ src/Components/pages/FifteenthPage.jsx
@@ -1,20 +1,23 @@
 import React from 'react'
 import letterPNG from "../../assets/letter.jpg";
-import kuromi1Gif from '../../assets/kuromi1.gif'
+import kuromi1Gif from '../../assets/testsa.png'
 
 const FifteenthPage = () => {
-  return (
-    <div className='h-full'>
+    return (
+        <div className='h-full'>
             <main className='h-full page-shadow overflow-hidden p-10'>
                 <div className='border-8 border-black'>
                     <img src={letterPNG} alt="cutie" />
                 </div>
-                <div className='w-full flex items-center justify-center'>
-                    <img width="50%" src={kuromi1Gif} alt="thanks" />
+                <div className='w-full h-full flex justify-center mt-10'>
+                    <div className='w-40 h-40 rounded-full flex items-center justify-center overflow-hidden'>
+                        <img className='w-full h-full object-cover' src={kuromi1Gif} alt="thanks" />
+                    </div>
                 </div>
+
             </main>
         </div>
-  )
+    )
 }
 
 export default FifteenthPage
